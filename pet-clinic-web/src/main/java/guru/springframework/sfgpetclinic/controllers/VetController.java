4e56da795a2d261c6c49359e62b44b6dfdf5707d
Mark Marcum <marcuma@gmail.com>
2022-04-11 09:31:52 -0400

    Listed all vets on vets/index.html. Closes #15.

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
@@ -1,5 +1,7 @@
 package guru.springframework.sfgpetclinic.controllers;
 
+import guru.springframework.sfgpetclinic.services.VetService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,8 +9,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    @Autowired
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+
     @GetMapping({"/vets", "/vets/index", "/vets/index.html"})
     public String listVets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         model.addAttribute("title", "Vets Index Page");
         return "vets/index";
     }
